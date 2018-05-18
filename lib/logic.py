892f2bd93c0d248a056496fabec21cb63f78444d
disazhar <39265310+disazhar@users.noreply.github.com>
2018-05-18 12:39:56 +0530

    voting logic

--- lib/logic.py
+++ lib/logic.py
@@ -1,1 +1,17 @@
+candidates = input("How many candidates do we have?\n")
+candidates = int(candidates)
+print("OK! So we have " + str(candidates) + " votable candidates.")
+print("Specify their names!\n")
 
+
+for candidates_number in range(candidates): 
+    print(int(candidates_number)) '''to help me visualize what i'm doing'''
+
+
+overall = candidates
+print(list(range(overall)))
+complete_list = range(overall)
+print(complete_list) '''to help me visualize what i'm doing'''
+
+for candidate_name in range(complete_list):
+    print("Write candidates names.\n")
